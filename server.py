f5d23d611dd4bf770a377d7934e778cdc9373f98
Victor Lue <vict.lue@gmail.com>
2021-11-14 17:54:16 -0500

    added new feature to server

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #
 #     DATABASEURI = "postgresql://gravano:foobar@34.74.246.148/proj1part2"
 #
-DATABASEURI = "postgresql://user:password@34.74.246.148/proj1part2"
+DATABASEURI = "postgresql://vl2420:6609@34.74.246.148/proj1part2"
 
 
 #
